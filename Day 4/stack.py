4840a6ca91f8f9ddbea2bd7e76a0193637d6ed7d
AyushDeuja <ayushdeuja11@gmail.com>
2026-01-09 21:18:06 +0545

    stack done in python

--- Day 4/stack.py
+++ Day 4/stack.py
@@ -6,9 +6,26 @@ class Stack:
         return len(self.s)
     
     def push(self, value):
-        self.s.append(value)
+        self.s.insert(0,value)
+
+    def peek(self):
+        # if self.length() == 0: // alternative way
+        if len(self.s) == 0:
+            raise Exception("Stack is empty")
+        else:
+            return self.s[0]
 
     def pop(self):
-        if self.isEmpty():
-            return "Stack is empty"
-        return self.s.pop()
+        if len(self.s) == 0:
+            raise Exception("Stack is empty")
+        else:
+            return self.s.pop(0)
+        
+stk = Stack()
+stk.push(10)
+stk.push(20)
+stk.push(30)
+# print(stk.peek())
+print(stk.pop())
+print(stk.pop())
+print(stk.pop())
